9572d9b4c8c5f4050dcabd9865ec02de03d546c3
devldm <39243060+devldm@users.noreply.github.com>
2025-07-13 17:49:58 +0900

    add redis caching

---
+++ redispkg/setup.go
@@ -0,0 +1,16 @@
+package redispkg
+
+import (
+	"github.com/redis/go-redis/v9"
+)
+
+func Connect() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+		Protocol: 2,
+	})
+
+	return client
+}
--- server/main.go
+++ server/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"grpc-loggy/redispkg"
 	"log"
 	"net"
 	"strings"
 
 	pb "grpc-loggy/proto/v1"
+
+	"github.com/redis/go-redis/v9"
 
 	"google.golang.org/grpc"
 )
@@ -19,6 +23,7 @@ type server struct {
 	pb.UnimplementedLoggyServiceServer
 	activeLogs  []*pb.Log
 	archiveLogs []*pb.Log
+	redisClient *redis.Client
 }
 
 func findSubstring(sliceStrings []*pb.Log, substring string) []*pb.Log {
@@ -32,12 +37,38 @@ func findSubstring(sliceStrings []*pb.Log, substring string) []*pb.Log {
 	return matches
 }
 
-func (s *server) SearchLogs(_ context.Context, in *pb.SearchLogsRequest) (*pb.SearchLogsResponse, error) {
+func (s *server) SearchLogs(ctx context.Context, in *pb.SearchLogsRequest) (*pb.SearchLogsResponse, error) {
 	log.Printf("Received %v", in.GetQuery())
+	cachedState, err := s.redisClient.HGet(ctx, string(in.GetQuery()), "logs").Result()
+	if err != nil {
+		if err.Error() != "redis: nil" {
+			log.Printf("HGet failed on cache key: %s, error: %v", in.GetQuery(), err)
+		}
+	}
+
+	if len(cachedState) > 0 {
+		fmt.Println("returning cached values")
+		var cachedData []*pb.Log
+		err := json.Unmarshal([]byte(cachedState), &cachedData)
+		if err != nil {
+			log.Printf("Failed to parse cached json: %v", err)
+		}
+		return &pb.SearchLogsResponse{TotalCount: int32(len(cachedData)), Log: cachedData}, nil
+	}
 	matches := findSubstring(s.activeLogs, in.GetQuery())
 	var res []*pb.Log
 	for _, l := range matches {
 		res = append(res, &pb.Log{Content: l.Content, Level: l.Level, Origin: l.Origin})
+	}
+
+	cacheMe, err := json.Marshal(res)
+	if err != nil {
+		log.Printf("Failed to marshal api response: %v", err)
+	}
+
+	err = s.redisClient.HSet(ctx, in.GetQuery(), "logs", cacheMe).Err()
+	if err != nil {
+		log.Printf("HSet failed: %v", err)
 	}
 	return &pb.SearchLogsResponse{TotalCount: int32(len(res)), Log: res}, nil
 }
@@ -66,7 +97,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterLoggyServiceServer(s, &server{})
+	pb.RegisterLoggyServiceServer(s, &server{redisClient: redispkg.Connect()})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
